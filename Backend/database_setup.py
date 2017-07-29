5a43489802d2bc9e63c1ca060de33792c5824a53
Rajendra Kadam <rajendrakadam249@gmail.com>
2017-07-29 15:12:59 +0530

    Add LessonPlan Class

---
+++ Backend/database_setup.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Pics(Base):
+
+	__tablename__ = "Pics"
+
+	id = Column(Integer, primary_key = True)
+	path = Column(String(100), unique = True, nullable = False)
+	lesson_id = Column(Integer, unique = False)
+
+class LessonPlan(Base):
+
+	__tablename__ = "LessonPlan"
+
+	id = Column(Integer, primary_key = True)
+	unit = Column(String(100), unique = False)
+	title = Column(String(100), unique = False)
+	purpose = Column(String(1000), unique = False)
+	class_value = Column(String(10), unique = False)
+	user_id = Column(Integer, unique = False)
+	description = Column(String(10000), unique = False, nullable = False)
+	timestamp = Column(Stirng(100), unique = False, nullable = False)
