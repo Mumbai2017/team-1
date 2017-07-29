62b352f568ce0d7aef9d118d2b5db1b40304a89d
Rajendra Kadam <rajendrakadam249@gmail.com>
2017-07-29 16:05:06 +0530

    Add tag table

--- Backend/database_setup.py
+++ Backend/database_setup.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.dialects.postgresql import ARRAY
 
 Base = declarative_base()
 
@@ -13,6 +12,7 @@ class Pics(Base):
 	id = Column(Integer, primary_key = True)
 	path = Column(String(100), unique = True, nullable = False)
 	lesson_id = Column(Integer, unique = False)
+
 
 class LessonPlan(Base):
 
@@ -46,8 +46,16 @@ class Videos(Base):
 	id = Column(Integer, primary_key = True)
 	user_id = Column(Integer, unique = False, nullable = False)
 	video_link = Column(String(100), unique = True, nullable = False)
-	tags = Column(ARRAY(String(100)), unique = False, nullable = True)
+	tags = Column(MutableList.as_mutable(ARRAY(String(100))))
 
+class Tag(Base):
+
+	__tablename__ = "Tag"
+
+	id = Column(Integer, primary_key = True)
+	tag_value = Column(String(100), unique = False, nullable = False)
+	video_id = Column(Integer, unique = False, nullable = False)
+	
 class Comment(Base):
 
 	__tablename__ = "Comment"
@@ -58,4 +66,9 @@ class Comment(Base):
 	video_id = Column(Integer, unique = False, nullable = False)
 	user_id = Column(Integer, unique = False, nullable = False)
 	parent_id = Column(Integer, unique = False, nullable = True)
-	
+
+engine = create_engine("sqlite:///codeforgood.db")
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+
