41c83fd19d08d4c1606f10c8a2c244700f28a327
savlakaran <savlakaran96@gmail.com>
2017-07-29 17:29:17 +0530

    commit to master

---
+++ Backend/application.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from database_setup import Pics, LessonPlan, User, Videos, Tag, Comment,DBSession 
+
+application = Flask(__name__)
+
+@application.route("/", methods = ["GET", "POST"])
+@application.route("/login", methods = ["GET", "POST"])
+def login():
+	try:
+		if request.method == "POST":
+			session = DBSession()
+			username = request.form["username"]
+			password_value = request.form["password"]
+			user = session.query(User).filter_by(email = username, password = password_value).first()
+			if user is not None:
+				print username
+				print password
+		else:
+			return render_template("login.html")
+	except Exception as e:
+		print e
+
+@application.route("/signup", methods = ["POST", "GET"])
+def signup():
+	try:
+		if request.method == "POST":
+			session = DBSession()
+			email_value = request.form["email"]
+			name_value = request.form["username"]
+			password_value = request.form["password"]
+			location_value = request.form["location"]
+			unit_preferred_value = request.form["unit"]
+			phone_number_value = request.form["phone"]
+			school_value = request.form["school"]
+			user = User(email = email_value,
+						name = name_value,
+						password = password_value,
+						location = location_value,
+						unit_preferred = unit_preferred_value,
+						school = school_value,
+						phone_number = phone_number_value
+						)
+		else:
+			return render_template("signup.html")
+	except Exception as e:
+		raise e 
+
+
+if __name__ == "__main__":
+	application.run(debug = True)
