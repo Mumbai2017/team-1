cc8467bda578444d98ea3322a0c6f04b2cbb2c3c
rajendra@hotstar.com <rajendra@hotstar.com>
2017-07-30 10:05:19 +0530

    indentation

--- application.py
+++ application.py
@@ -33,8 +33,8 @@ def login():
 			if username == "admin@email.com" and request.form["password"] == "admin":
 				session = DBSession()
 				video = session.query(Videos).all()
-                user_admin = User()
-                user_admin.id = 25
+				user_admin = User()
+				user_admin.id = 25
                 login_user(user)
 				return render_template("admin_dashboard.html", videos = video)
 			elif user is not None and password_value == user.password:
